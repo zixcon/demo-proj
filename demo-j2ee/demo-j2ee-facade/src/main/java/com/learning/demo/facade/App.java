99ac38cccee6225d0d058fca8f8cf3fa10ccbe36
zixcon <zixcon.fan@outlook.com>
2017-06-18 20:35:26 +0800

    add j2ee demo proj

---
+++ demo-j2ee/demo-j2ee-facade/src/main/java/com/learning/demo/facade/App.java
@@ -0,0 +1,13 @@
+package com.learning.demo.facade;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
