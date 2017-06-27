0b859d718e757718341db63d267613dcc55f0bac
zixcon <zixcon.fan@outlook.com>
2017-06-27 21:05:26 +0800

    swagger2 需要在spring mvc中配置启用，原因是配置类必须和mvc处于同一个上下文中

--- demo-j2ee/demo-j2ee-web/src/main/java/com/learning/demo/web/global/swagger/SwaggerConfig.java
+++ demo-j2ee/demo-j2ee-web/src/main/java/com/learning/demo/web/global/swagger/SwaggerConfig.java
@@ -1,28 +1,48 @@
 package com.learning.demo.web.global.swagger;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.WebApplicationContext;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 访问Swagger的api列表页面
- * http://localhost:8080/${contextpath}/swagger-ui.html
+ * http://{ip}:{port}/{projectname}/swagger-ui.html#/
+ *
+ * 需要在spring mvc中配置启用，原因是配置类必须和mvc处于同一个上下文中
+ * http://blog.csdn.net/qq_25615395/article/details/70229139
+ *
  * Created by topaz on 2017/6/26.
  */
-/*@Configuration
+// @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
+    public Docket buildDocket(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(buildApiInf())
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.learning.demo.web.controller"))//controller路径
                 .paths(PathSelectors.any())
                 .build();
     }
-}*/
+
+    private ApiInfo buildApiInf(){
+        return new ApiInfoBuilder()
+                .title("demo-proj")
+                .termsOfServiceUrl("http://blog.csdn.net/qq_25615395/article/details/70229139")
+                .description("swagger2")
+                .contact(new Contact("demo-proj", "http://127.0.0.1:8080/demo", "zixcon.fan@outlook.com"))
+                .build();
+
+    }
+}
