d1deba21e4deed31be8d993bf99f2fe95fb40a6c
zixcon <zixcon.fan@outlook.com>
2017-07-06 22:30:40 +0800

    add comment to plugin

--- demo-common/mybatis-generate-plugin/src/main/java/com/learning/common/mybatis/generator/CustomCommentGenerator.java
+++ demo-common/mybatis-generate-plugin/src/main/java/com/learning/common/mybatis/generator/CustomCommentGenerator.java
@@ -1,25 +1,23 @@
 package com.learning.common.mybatis.generator;
 
-import com.learning.common.mybatis.plugin.PaginationPlugin;
-import org.mybatis.generator.api.*;
-import org.mybatis.generator.api.dom.java.*;
-import org.mybatis.generator.api.dom.xml.XmlElement;
-import org.mybatis.generator.config.Configuration;
+import com.google.common.base.Charsets;
+import com.google.common.base.Joiner;
+import javafx.util.converter.LocalDateStringConverter;
+import org.joda.time.format.DateTimeFormat;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.InnerClass;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
 import org.mybatis.generator.config.PropertyRegistry;
-import org.mybatis.generator.config.xml.ConfigurationParser;
-import org.mybatis.generator.exception.InvalidConfigurationException;
-import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultCommentGenerator;
-import org.mybatis.generator.internal.DefaultShellCallback;
-import org.mybatis.generator.internal.util.StringUtility;
 
-import java.io.File;
-import java.io.IOException;
-import java.sql.SQLException;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.List;
 import java.util.Properties;
 
 import static org.mybatis.generator.internal.util.StringUtility.isTrue;
@@ -28,114 +26,67 @@ import static org.mybatis.generator.internal.util.StringUtility.isTrue;
  * 自定义注解
  * Created by topaz on 2017/7/1.
  */
-public class CustomCommentGenerator implements CommentGenerator{
+public class CustomCommentGenerator extends DefaultCommentGenerator{
 
     private static final String STR_SEPRATOR = " * ";
 
-    private Properties properties;
-    private Properties systemPro;
-    private boolean suppressDate;
-    private boolean suppressAllComments;
-    private String currentDateStr;
-    private boolean addRemarkComments;
-
-    public CustomCommentGenerator() {
-        super();
-        properties = new Properties();
-        systemPro = System.getProperties();
-        suppressDate = false;
-        suppressAllComments = false;
-        currentDateStr = (new SimpleDateFormat("yyyy-MM-dd")).format(new Date());
-    }
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
         field.addJavaDocLine("/**");
-        field.addJavaDocLine(STR_SEPRATOR.concat("最大长度：").concat(Integer.toString(introspectedColumn.getLength())));
-        field.addJavaDocLine(STR_SEPRATOR.concat("字段说明：").concat(introspectedColumn.getRemarks()));
-        field.addJavaDocLine(STR_SEPRATOR.concat("默认值 ：").concat(introspectedColumn.getDefaultValue()));
-        field.addJavaDocLine(STR_SEPRATOR.concat("允许null：").concat(Boolean.toString(introspectedColumn.isNullable())));
+        StringBuilder sb = new StringBuilder();
+        sb.append(STR_SEPRATOR);
+        sb.append("最大长度：");
+        sb.append(Integer.toString(introspectedColumn.getLength()));
+        field.addJavaDocLine(sb.toString());
+        sb.setLength(0);
+        sb.append(STR_SEPRATOR);
+        sb.append("字段说明：");
+        sb.append(introspectedColumn.getRemarks());
+        field.addJavaDocLine(sb.toString());
+        sb.setLength(0);
+        sb.append(STR_SEPRATOR);
+        sb.append("默认值 ：");
+        sb.append(introspectedColumn.getDefaultValue());
+        field.addJavaDocLine(sb.toString());sb.setLength(0);
+        sb.append(STR_SEPRATOR);
+        sb.append("允许null：");
+        sb.append(Boolean.toString(introspectedColumn.isNullable()));
+        field.addJavaDocLine(sb.toString());
         field.addJavaDocLine(" */");
     }
 
     public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
+        this.addClassComment(innerClass,introspectedTable,false);
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
         innerClass.addJavaDocLine("/**");
-        innerClass.addJavaDocLine(STR_SEPRATOR.concat(introspectedTable.getRemarks()));
-        innerClass.addJavaDocLine(" */");
-    }
-
-    @Override
-    public void addConfigurationProperties(Properties properties) {
-        this.properties.putAll(properties);
-
-        suppressDate = isTrue(properties
-                .getProperty(PropertyRegistry.COMMENT_GENERATOR_SUPPRESS_DATE));
-
-        suppressAllComments = isTrue(properties
-                .getProperty(PropertyRegistry.COMMENT_GENERATOR_SUPPRESS_ALL_COMMENTS));
-
-        addRemarkComments = isTrue(properties
-                .getProperty(PropertyRegistry.COMMENT_GENERATOR_ADD_REMARK_COMMENTS));
-    }
-
-    @Override
-    public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
-
+        StringBuilder sb = new StringBuilder();
+        sb.append(STR_SEPRATOR);
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        innerClass.addJavaDocLine(sb.toString());
+        sb.setLength(0);
+        sb.append(STR_SEPRATOR);
+        sb.append(introspectedTable.getRemarks());
+        innerClass.addJavaDocLine(sb.toString());
+        sb.setLength(0);
+        sb.append(STR_SEPRATOR);
+        sb.append("创建于：");
+        sb.append(dateFormat.format(new Date()));
+        innerClass.addJavaDocLine(sb.toString());
+        innerClass.addJavaDocLine("*/");
     }
 
     @Override
     public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-
-    }
-
-    @Override
-    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean b) {
-
-    }
-
-    @Override
-    public void addEnumComment(InnerEnum innerEnum, IntrospectedTable introspectedTable) {
-
-    }
-
-    @Override
-    public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
-
-    }
-
-    @Override
-    public void addSetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
-
+        this.addClassComment(topLevelClass,introspectedTable,false);
     }
 
     @Override
     public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
-
+        //super.addGeneralMethodComment(method, introspectedTable);
     }
-
-    @Override
-    public void addJavaFileComment(CompilationUnit compilationUnit) {
-
-    }
-
-    @Override
-    public void addComment(XmlElement xmlElement) {
-
-    }
-
-    @Override
-    public void addRootComment(XmlElement xmlElement) {
-
-    }
-
-    /*
-    public static void main(String[] args) {
-
-        String config= CustomCommentGenerator.class.getClassLoader().getResource("generatorConfig.xml").getFile();
-
-        String[] arg= { "-configfile", config, "-overwrite"};
-
-        ShellRunner.main(arg);
-
-    }*/
 
 }
