bc099508eaf1ea064a22a58338f095d56089a2a4
zixcon <zixcon.fan@outlook.com>
2017-07-24 23:28:33 +0800

    rm .mvn

---
+++ demo-boot/demo-boot-parent/demo-boot-jar/src/main/java/com/learning/boot/jar/DemoBootJarApplication.java
@@ -0,0 +1,12 @@
+package com.learning.boot.jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBootJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBootJarApplication.class, args);
+	}
+}
